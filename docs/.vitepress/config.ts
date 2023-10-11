943ebe9f8d008347952587f25521b5c07375cf02
nexthis <pawel.romanowski000@gmail.com>
2023-10-11 16:45:14 +0200

    set base path

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Directus Stripe",
   description: "Directus stripe plugin",
+  base: "/directus-stripe/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
